4c3e6b7f92812f1926de7cff9a8d9a5819226d30
daana <126dpelaezayala@frhsd.com>
2024-12-03 12:53:49 -0500

    Message

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,6 +1,38 @@
 var Username = "name";
+let textbox= ""
+document.getElementById=("search");
+document.getElementById=("button").addeventfunction('click'.a()=> {
+
+}
+console.log(text.content);
 console.log("hello");
 var userQuestion ="question";
 console.log( userQuestion);
+
 var randomNumber = (Math.random() * 8);
 var  eightBall =" ";
+// If/else statements 
+if (randomNumber=1) {
+    userQuestion="It is decidedly so"
+    console.log('working');
+} else if (randomNumber=2) {
+    userQuestion="It is decidedly so"
+    console.log('working');
+} else if (randomNumber=3) {
+    userQuestion="Reply hazy try again"
+    console.log('working');
+} else if (randomNumber=4) {
+    userQuestion="Cannot predict now"
+    console.log('working');
+} else if (randomNumber=5) {
+    userQuestion="Do not count on it"
+    console.log('working');
+} else if (randomNumber=6) {
+    userQuestion="My sources say no"
+    console.log('working');
+} else if (randomNumber=7) {
+    userQuestion("Outlook not so good")
+} else if (randomNumber=8) {
+    userQuestion("Signs point to yes")
+}
+console.log(eightBall);
