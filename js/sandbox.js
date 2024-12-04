596418569df5ee27b7c8aaafc7884d48f5bf4610
daana <126dpelaezayala@frhsd.com>
2024-12-04 14:00:05 -0500

    Message

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,38 +1,46 @@
-var Username = "name";
-let textbox= ""
-document.getElementById=("search");
-document.getElementById=("button").addeventfunction('click'.a()=> {
+var Username = "";
+let textbox = document.getElementById("search");
+document.getElementById("button").addEventListener('click', () => {
+    if (Username = "") {
+        Username = (textbox.value);
+        textbox.value = "";
+        textbox.placeholder = "Ask your question";
 
-}
-console.log(text.content);
-console.log("hello");
-var userQuestion ="question";
-console.log( userQuestion);
+    } else {
 
-var randomNumber = (Math.random() * 8);
-var  eightBall =" ";
-// If/else statements 
-if (randomNumber=1) {
-    userQuestion="It is decidedly so"
-    console.log('working');
-} else if (randomNumber=2) {
-    userQuestion="It is decidedly so"
-    console.log('working');
-} else if (randomNumber=3) {
-    userQuestion="Reply hazy try again"
-    console.log('working');
-} else if (randomNumber=4) {
-    userQuestion="Cannot predict now"
-    console.log('working');
-} else if (randomNumber=5) {
-    userQuestion="Do not count on it"
-    console.log('working');
-} else if (randomNumber=6) {
-    userQuestion="My sources say no"
-    console.log('working');
-} else if (randomNumber=7) {
-    userQuestion("Outlook not so good")
-} else if (randomNumber=8) {
-    userQuestion("Signs point to yes")
-}
-console.log(eightBall);
+        var randomNumber = Math.floor(Math.random() * 8);
+        console.log(randomNumber)
+        var eightBall = " ";
+        // If/else statements 
+        if (randomNumber == 1) {
+            userQuestion = "It is certain"
+
+        } else if (randomNumber ==2) {
+            userQuestion = "It is decidedly so"
+
+        } else if (randomNumber ==3) {
+            userQuestion = "Reply hazy try again"
+
+        } else if (randomNumber == 4) {
+            userQuestion = "Cannot predict now"
+
+        } else if (randomNumber == 5) {
+            userQuestion = "Do not count on it"
+
+        } else if (randomNumber == 6) {
+            userQuestion = "My sources say no"
+
+        } else if (randomNumber == 7) {
+            userQuestion="Outlook not so good"
+        } else if (randomNumber == 8) {
+            userQuestion="Signs point to yes"
+        }
+        console.log(userQuestion);
+
+    }
+
+});
+
+    console.log("hello");
+
+
